d1952ef22cb413abac83079d91c5c402e79dd66b
Jose <Jose@DESKTOP-5H3RN8M>
2021-12-28 17:39:26 +0100

    Agregar archivos de proyecto.

---
+++ CatálogoPS5/Form1.Designer.vb
@@ -0,0 +1,272 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PanelMenu = New System.Windows.Forms.Panel()
+        Me.IconButton6 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton5 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton4 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton1 = New FontAwesome.Sharp.IconButton()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.IconButton3 = New FontAwesome.Sharp.IconButton()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PanelTitleBar = New System.Windows.Forms.Panel()
+        Me.IconButton2 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton7 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton8 = New FontAwesome.Sharp.IconButton()
+        Me.PanelDesktop = New System.Windows.Forms.Panel()
+        Me.PanelMenu.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelTitleBar.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PanelMenu
+        '
+        Me.PanelMenu.BackColor = System.Drawing.Color.MidnightBlue
+        Me.PanelMenu.Controls.Add(Me.IconButton6)
+        Me.PanelMenu.Controls.Add(Me.IconButton5)
+        Me.PanelMenu.Controls.Add(Me.IconButton4)
+        Me.PanelMenu.Controls.Add(Me.IconButton1)
+        Me.PanelMenu.Controls.Add(Me.Panel1)
+        Me.PanelMenu.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PanelMenu.Location = New System.Drawing.Point(0, 0)
+        Me.PanelMenu.Name = "PanelMenu"
+        Me.PanelMenu.Size = New System.Drawing.Size(132, 417)
+        Me.PanelMenu.TabIndex = 4
+        '
+        'IconButton6
+        '
+        Me.IconButton6.BackColor = System.Drawing.Color.MidnightBlue
+        Me.IconButton6.FlatAppearance.BorderSize = 0
+        Me.IconButton6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton6.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.IconButton6.IconChar = FontAwesome.Sharp.IconChar.SignOutAlt
+        Me.IconButton6.IconColor = System.Drawing.Color.White
+        Me.IconButton6.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton6.IconSize = 40
+        Me.IconButton6.Location = New System.Drawing.Point(47, 370)
+        Me.IconButton6.Name = "IconButton6"
+        Me.IconButton6.Size = New System.Drawing.Size(39, 39)
+        Me.IconButton6.TabIndex = 5
+        Me.IconButton6.UseVisualStyleBackColor = False
+        '
+        'IconButton5
+        '
+        Me.IconButton5.BackColor = System.Drawing.Color.MidnightBlue
+        Me.IconButton5.FlatAppearance.BorderSize = 0
+        Me.IconButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton5.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.IconButton5.IconChar = FontAwesome.Sharp.IconChar.StarHalfAlt
+        Me.IconButton5.IconColor = System.Drawing.Color.White
+        Me.IconButton5.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton5.IconSize = 40
+        Me.IconButton5.Location = New System.Drawing.Point(47, 219)
+        Me.IconButton5.Name = "IconButton5"
+        Me.IconButton5.Size = New System.Drawing.Size(39, 39)
+        Me.IconButton5.TabIndex = 4
+        Me.IconButton5.UseVisualStyleBackColor = False
+        '
+        'IconButton4
+        '
+        Me.IconButton4.BackColor = System.Drawing.Color.MidnightBlue
+        Me.IconButton4.FlatAppearance.BorderSize = 0
+        Me.IconButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton4.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.IconButton4.IconChar = FontAwesome.Sharp.IconChar.Search
+        Me.IconButton4.IconColor = System.Drawing.Color.White
+        Me.IconButton4.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton4.IconSize = 40
+        Me.IconButton4.Location = New System.Drawing.Point(47, 153)
+        Me.IconButton4.Name = "IconButton4"
+        Me.IconButton4.Size = New System.Drawing.Size(39, 39)
+        Me.IconButton4.TabIndex = 3
+        Me.IconButton4.UseVisualStyleBackColor = False
+        '
+        'IconButton1
+        '
+        Me.IconButton1.BackColor = System.Drawing.Color.MidnightBlue
+        Me.IconButton1.FlatAppearance.BorderSize = 0
+        Me.IconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton1.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.IconButton1.IconChar = FontAwesome.Sharp.IconChar.Home
+        Me.IconButton1.IconColor = System.Drawing.Color.White
+        Me.IconButton1.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton1.IconSize = 40
+        Me.IconButton1.Location = New System.Drawing.Point(47, 85)
+        Me.IconButton1.Name = "IconButton1"
+        Me.IconButton1.Size = New System.Drawing.Size(39, 39)
+        Me.IconButton1.TabIndex = 1
+        Me.IconButton1.UseVisualStyleBackColor = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.IconButton3)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(132, 69)
+        Me.Panel1.TabIndex = 0
+        '
+        'IconButton3
+        '
+        Me.IconButton3.FlatAppearance.BorderSize = 0
+        Me.IconButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton3.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.IconButton3.IconChar = FontAwesome.Sharp.IconChar.Bars
+        Me.IconButton3.IconColor = System.Drawing.Color.White
+        Me.IconButton3.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton3.IconSize = 30
+        Me.IconButton3.Location = New System.Drawing.Point(86, 3)
+        Me.IconButton3.Name = "IconButton3"
+        Me.IconButton3.Size = New System.Drawing.Size(46, 46)
+        Me.IconButton3.TabIndex = 2
+        Me.IconButton3.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.CatálogoPS5.My.Resources.Resources.consoles_ps_256
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(44, 34)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'PanelTitleBar
+        '
+        Me.PanelTitleBar.BackColor = System.Drawing.Color.Black
+        Me.PanelTitleBar.Controls.Add(Me.IconButton2)
+        Me.PanelTitleBar.Controls.Add(Me.IconButton7)
+        Me.PanelTitleBar.Controls.Add(Me.IconButton8)
+        Me.PanelTitleBar.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelTitleBar.Location = New System.Drawing.Point(132, 0)
+        Me.PanelTitleBar.Name = "PanelTitleBar"
+        Me.PanelTitleBar.Size = New System.Drawing.Size(613, 37)
+        Me.PanelTitleBar.TabIndex = 5
+        '
+        'IconButton2
+        '
+        Me.IconButton2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.IconButton2.BackColor = System.Drawing.Color.Black
+        Me.IconButton2.FlatAppearance.BorderSize = 0
+        Me.IconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton2.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.IconButton2.IconChar = FontAwesome.Sharp.IconChar.Circle
+        Me.IconButton2.IconColor = System.Drawing.Color.FromArgb(CType(CType(240, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(108, Byte), Integer))
+        Me.IconButton2.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton2.IconSize = 20
+        Me.IconButton2.Location = New System.Drawing.Point(513, 0)
+        Me.IconButton2.Name = "IconButton2"
+        Me.IconButton2.Size = New System.Drawing.Size(38, 31)
+        Me.IconButton2.TabIndex = 3
+        Me.IconButton2.UseVisualStyleBackColor = False
+        '
+        'IconButton7
+        '
+        Me.IconButton7.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.IconButton7.BackColor = System.Drawing.Color.Black
+        Me.IconButton7.FlatAppearance.BorderSize = 0
+        Me.IconButton7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton7.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.IconButton7.IconChar = FontAwesome.Sharp.IconChar.Square
+        Me.IconButton7.IconColor = System.Drawing.Color.FromArgb(CType(CType(213, Byte), Integer), CType(CType(145, Byte), Integer), CType(CType(189, Byte), Integer))
+        Me.IconButton7.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton7.IconSize = 20
+        Me.IconButton7.Location = New System.Drawing.Point(548, 0)
+        Me.IconButton7.Name = "IconButton7"
+        Me.IconButton7.Size = New System.Drawing.Size(37, 31)
+        Me.IconButton7.TabIndex = 4
+        Me.IconButton7.UseVisualStyleBackColor = False
+        '
+        'IconButton8
+        '
+        Me.IconButton8.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.IconButton8.BackColor = System.Drawing.Color.Black
+        Me.IconButton8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.IconButton8.FlatAppearance.BorderSize = 0
+        Me.IconButton8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton8.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.IconButton8.IconChar = FontAwesome.Sharp.IconChar.Times
+        Me.IconButton8.IconColor = System.Drawing.Color.FromArgb(CType(CType(155, Byte), Integer), CType(CType(173, Byte), Integer), CType(CType(228, Byte), Integer))
+        Me.IconButton8.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton8.IconSize = 20
+        Me.IconButton8.Location = New System.Drawing.Point(581, 0)
+        Me.IconButton8.Name = "IconButton8"
+        Me.IconButton8.Size = New System.Drawing.Size(32, 31)
+        Me.IconButton8.TabIndex = 5
+        Me.IconButton8.UseVisualStyleBackColor = False
+        '
+        'PanelDesktop
+        '
+        Me.PanelDesktop.BackColor = System.Drawing.Color.Silver
+        Me.PanelDesktop.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelDesktop.Location = New System.Drawing.Point(132, 37)
+        Me.PanelDesktop.Name = "PanelDesktop"
+        Me.PanelDesktop.Size = New System.Drawing.Size(613, 380)
+        Me.PanelDesktop.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(745, 417)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.PanelDesktop)
+        Me.Controls.Add(Me.PanelTitleBar)
+        Me.Controls.Add(Me.PanelMenu)
+        Me.Font = New System.Drawing.Font("Palatino Linotype", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.Name = "Form1"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.PanelMenu.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelTitleBar.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PanelMenu As Panel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PanelTitleBar As Panel
+    Friend WithEvents PanelDesktop As Panel
+    Friend WithEvents IconButton1 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton6 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton5 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton4 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton3 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton2 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton7 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton8 As FontAwesome.Sharp.IconButton
+End Class
---
+++ CatálogoPS5/Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ToolStripComboBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs)
+
+    End Sub
+
+    Private Sub ArchivoToolStripMenuItem_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+    Private Sub TextBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub IconButton1_Click(sender As Object, e As EventArgs) Handles IconButton1.Click
+
+    End Sub
+
+    Private Sub IconButton8_Click(sender As Object, e As EventArgs) Handles IconButton8.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub IconButton2_Click(sender As Object, e As EventArgs) Handles IconButton2.Click
+        Me.WindowState = WindowState.Minimized
+
+    End Sub
+
+    Private Sub IconButton7_Click(sender As Object, e As EventArgs) Handles IconButton7.Click
+
+    End Sub
+End Class
